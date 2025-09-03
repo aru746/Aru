ee26256c7f13954d7354dbc18e47fd46c95c0708
aru746 <naruto1629u@gmail.com>
2025-09-03 22:00:24 +0530

    Update setleave.js

--- scripts/cmds/setleave.js
+++ scripts/cmds/setleave.js
@@ -12,7 +12,7 @@ module.exports = {
 			vi: "Chỉnh sửa nội dung/bật/tắt tin nhắn tạm biệt thành viên rời khỏi nhóm chat của bạn",
 			en: "Edit content/turn on/off leave message when member leave your group chat"
 		},
-		category: "custom",
+		category: "owner",
 		guide: {
 			vi: {
 				body: "   {pn} on: Bật tin nhắn tạm biệt"
