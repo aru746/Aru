aa01af30d8e362de1393cead68cf1338462914d4
aru746 <naruto1629u@gmail.com>
2025-09-03 22:21:05 +0530

    Update setrole.js

--- scripts/cmds/setrole.js
+++ scripts/cmds/setrole.js
@@ -9,7 +9,7 @@ module.exports = {
 			vi: "Chỉnh sửa role của lệnh (những lệnh có role < 2)",
 			en: "Edit role of command (commands with role < 2)"
 		},
-		category: "info",
+		category: "admin",
 		guide: {
 			vi: "   {pn} <commandName> <new role>: set role mới cho lệnh"
 				+ "\n   Với:"
